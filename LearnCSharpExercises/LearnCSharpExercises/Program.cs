15761740743366598abdf1d35d58a41395217ac6
bal3gj <dvsh24@gmail.com>
2025-01-23 13:15:26 +0900

    add: No00

--- LearnCSharpExercises/LearnCSharpExercises/Program.cs
+++ LearnCSharpExercises/LearnCSharpExercises/Program.cs
@@ -8,8 +8,17 @@ namespace LearnCSharpExercises
 {
     internal class Program
     {
+        //No. 00 ごあいさつ
+        //実行するとHello World!と表示するプログラムを作成せよ。
+
+        //【実行例】
+        //$ ./ knock00
+        //Hello World!
+        //$
+
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World!");
         }
     }
 }
