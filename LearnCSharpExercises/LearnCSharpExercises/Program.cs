40d4142c6a65dfbed58df835441923f558010401
bal3gj <dvsh24@gmail.com>
2025-01-23 13:30:35 +0900

    add: No01

--- LearnCSharpExercises/LearnCSharpExercises/Program.cs
+++ LearnCSharpExercises/LearnCSharpExercises/Program.cs
@@ -8,8 +8,19 @@ namespace LearnCSharpExercises
 {
     internal class Program
     {
+        //No. 01 足し算
+        //12345+23456を計算して結果を表示するプログラムを作成せよ。
+
+        //【実行例】
+        //$ ./knock01
+        //12345 + 23456 = 35801
+        //$
+
         static void Main(string[] args)
         {
+            int a = 12345;
+            int b = 23456;
+            Console.WriteLine($"{a} + {b} = {a + b}");
         }
     }
 }
