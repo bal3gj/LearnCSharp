1f44cb77a34868773b3cef661c1ec40007e14eb4
bal3gj <dvsh24@gmail.com>
2025-01-23 13:38:07 +0900

    add: No03

--- LearnCSharpExercises/LearnCSharpExercises/Program.cs
+++ LearnCSharpExercises/LearnCSharpExercises/Program.cs
@@ -8,8 +8,20 @@ namespace LearnCSharpExercises
 {
     internal class Program
     {
+        //No. 03 入力
+        //整数値を入力させ、その入力値を表示するプログラムを作成せよ。
+
+        //【実行例、下線部は入力例】
+        //$ ./knock03
+        //input number: 123
+        //your number is 123
+        //$
+
         static void Main(string[] args)
         {
+            Console.Write("input number: ");
+            string input = Console.ReadLine();
+            Console.WriteLine($"your number is {input}");
         }
     }
 }
