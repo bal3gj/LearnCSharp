a3bcf6b3cbd3aeb0f884b17db1c79b2534638af1
bal3gj <dvsh24@gmail.com>
2025-01-23 13:32:44 +0900

    add: No02

--- LearnCSharpExercises/LearnCSharpExercises/Program.cs
+++ LearnCSharpExercises/LearnCSharpExercises/Program.cs
@@ -8,8 +8,21 @@ namespace LearnCSharpExercises
 {
     internal class Program
     {
+
+        //No. 02 余り
+        //12345を7で割った余りを表示するプログラムを作成せよ。
+
+        //【実行例】
+        //$ ./knock02
+        //12345 ÷ 7 の余りは 4
+        //$
+        //変数は使用してもしなくてもよい。
+
         static void Main(string[] args)
         {
+            int a = 12345;
+            int b = 7;
+            Console.WriteLine($"{a} ÷ {b} の余りは {a % b}");
         }
     }
 }
