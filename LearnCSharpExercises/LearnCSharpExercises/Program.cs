be7319b4d35ea0de2aae412febc1796dab4a1533
bal3gj <dvsh24@gmail.com>
2025-01-31 14:30:34 +0900

    add: No11

--- LearnCSharpExercises/LearnCSharpExercises/Program.cs
+++ LearnCSharpExercises/LearnCSharpExercises/Program.cs
@@ -8,8 +8,30 @@ namespace LearnCSharpExercises
 {
     internal class Program
     {
+
+        //No. 11 ごあいさつ10回
+        //Hello World!を10回繰り返して表示するプログラムを作成せよ。
+
+        //【実行例、下線部は入力例】
+        //$ ./knock11
+        //Hello World!
+        //Hello World!
+        //Hello World!
+        //Hello World!
+        //Hello World!
+        //Hello World!
+        //Hello World!
+        //Hello World!
+        //Hello World!
+        //Hello World!
+        //$
+
         static void Main(string[] args)
         {
-        }
+            for(int i=0;i<10;i++)
+            {
+                Console.WriteLine("Hello World!");
+            }
+        } // 出力結果を確認する場合はこの行にブレークポイントを設定してデバッグ実行すると良いですよ
     }
 }
