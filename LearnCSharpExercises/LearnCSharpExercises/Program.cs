2fe914aefe3d2a1132ade4a7f1b84c5ca5f7d917
bal3gj <dvsh24@gmail.com>
2025-01-31 14:07:45 +0900

    add: No09

--- LearnCSharpExercises/LearnCSharpExercises/Program.cs
+++ LearnCSharpExercises/LearnCSharpExercises/Program.cs
@@ -8,8 +8,58 @@ namespace LearnCSharpExercises
 {
     internal class Program
     {
+        //No. 09 正? 負? 0?
+        //整数値を入力させ、値が正であればpositive、負であればnegative、0であればzeroと表示するプログラムを作成せよ。
+
+        //【実行例、下線部は入力例】
+        //$ ./knock09
+        //input number: 1
+        //positive
+        //$ ./knock09
+        //input number: -1
+        //negative
+        //$ ./knock09
+        //input number: 0
+        //zero
+        //$
+
         static void Main(string[] args)
         {
+            while (true)
+            {
+                Console.Write("input number: ");
+                var val = Console.ReadLine();
+
+                if (val == "quit") // quit と入力されたら終了
+                {
+                    break;
+                }
+
+                int num;
+
+                if (int.TryParse(val, out num) == false)
+                {
+                    Console.WriteLine("整数を入力してください。");
+                    continue;
+                }
+
+                if (num > 0)
+                {
+                    Console.WriteLine("positive");
+                }
+                else if (num < 0)
+                {
+                    Console.WriteLine("negative");
+                }
+                else if (num == 0)
+                {
+                    Console.WriteLine("zero");
+                }
+                else
+                {
+                    // NOP
+                }
+            }
         }
     }
 }
