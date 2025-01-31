6133c317ee4e04c061557b0b3dfadc25afe9746a
bal3gj <dvsh24@gmail.com>
2025-01-31 14:41:21 +0900

    add: No15

--- LearnCSharpExercises/LearnCSharpExercises/Program.cs
+++ LearnCSharpExercises/LearnCSharpExercises/Program.cs
@@ -8,8 +8,39 @@ namespace LearnCSharpExercises
 {
     internal class Program
     {
+
+        //No. 15 2ずつカウントアップ
+        //整数値を入力させ、0から入力値を超えない値まで2ずつ増やして表示するプログラムを作成せよ。
+
+        //【実行例、下線部は入力例】
+        //$ ./knock15
+        //input number: 7
+        //0
+        //2
+        //4
+        //6
+        //$
+
         static void Main(string[] args)
         {
+            int num = 0;
+
+            while (num <= 0)
+            {
+                Console.Write("input number: ");
+                var val = Console.ReadLine();
+
+                if (int.TryParse(val, out num) == false || num <= 0)
+                {
+                    Console.WriteLine("1以上の整数を入力してください。");
+                    continue;
+                }
+            }
+
+            for (int i = 0; i <= num; i+=2)
+            {
+                Console.WriteLine($"{i}");
+            }
         }
     }
 }
