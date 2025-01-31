64c512ef1a3622bd0b932d24deef55fd9ae6ab98
bal3gj <dvsh24@gmail.com>
2025-01-31 14:38:02 +0900

    add: No13

--- LearnCSharpExercises/LearnCSharpExercises/Program.cs
+++ LearnCSharpExercises/LearnCSharpExercises/Program.cs
@@ -8,8 +8,41 @@ namespace LearnCSharpExercises
 {
     internal class Program
     {
+
+        //整数値を入力させ、0から入力値まで数を1ずつ増やして表示するプログラムを作成せよ。
+
+        //【実行例、下線部は入力例】
+        //$ ./knock13
+        //input number: 5
+        //0
+        //1
+        //2
+        //3
+        //4
+        //5
+        //$
+        //入力値に0以下の値を入力した場合は考慮しなくてもよい。
+
         static void Main(string[] args)
         {
+            int num = 0;
+
+            while (num <= 0)
+            {
+                Console.Write("input number: ");
+                var val = Console.ReadLine();
+
+                if (int.TryParse(val, out num) == false || num <= 0)
+                {
+                    Console.WriteLine("1以上の整数を入力してください。");
+                    continue;
+                }
+            }
+
+            for (int i = 0; i <= num; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
         }
     }
 }
