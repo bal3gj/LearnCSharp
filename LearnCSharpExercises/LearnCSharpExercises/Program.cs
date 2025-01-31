472a93f0632fa60cce466a7e1c837bce702f2524
bal3gj <dvsh24@gmail.com>
2025-01-31 13:56:28 +0900

    add: No06

--- LearnCSharpExercises/LearnCSharpExercises/Program.cs
+++ LearnCSharpExercises/LearnCSharpExercises/Program.cs
@@ -8,8 +8,33 @@ namespace LearnCSharpExercises
 {
     internal class Program
     {
+        //No. 06 0?
+        //整数値を入力させ、値が0ならzeroと表示するプログラムを作成せよ。
+
+        //【実行例、下線部は入力例】
+        //$ ./knock06
+        //input number: 0
+        //zero
+        //$ ./knock06
+        //input number: 1
+        //$
+
         static void Main(string[] args)
         {
+            while (true)
+            {
+                Console.Write("input number: ");
+                var val = Console.ReadLine();
+
+                if(val == "0")
+                {
+                    Console.WriteLine("zero");
+                }
+                else if(val == "quit") // quit と入力されたら終了
+                {
+                    break;
+                }
+            }
         }
     }
 }
