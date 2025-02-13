e87c570d970d95befe10e1550c0ee312ba5e0916
bal3gj <dvsh24@gmail.com>
2025-02-13 11:47:56 +0900

    add: No16

--- LearnCSharpExercises/LearnCSharpExercises/Program.cs
+++ LearnCSharpExercises/LearnCSharpExercises/Program.cs
@@ -8,8 +8,33 @@ namespace LearnCSharpExercises
 {
     internal class Program
     {
+
+        //No. 16 0でおしまい
+        //整数値を入力させ、入力値が0でなければ再度入力させ、0であれば終了するプログラムを作成せよ。
+
+        //【実行例、下線部は入力例】
+        //$ ./knock16
+        //input number: 5
+        //input number: 3
+        //input number: -4
+        //input number: 0
+        //$
+        //do-while文を使う、あるいは無限ループをbreak文で抜け出す。
+
         static void Main(string[] args)
         {
+            int num = -1;
+
+            while (num != 0)
+            {
+                Console.Write("input number: ");
+                var val = Console.ReadLine();
+
+                if (int.TryParse(val, out num) == true && num == 0)
+                {
+                    break;
+                }
+            }
         }
     }
 }
