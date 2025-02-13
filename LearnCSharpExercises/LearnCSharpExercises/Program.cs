cc504479575603f73a57333847f8bba08a2db70d
bal3gj <dvsh24@gmail.com>
2025-02-13 12:37:46 +0900

    add: No18

--- LearnCSharpExercises/LearnCSharpExercises/Program.cs
+++ LearnCSharpExercises/LearnCSharpExercises/Program.cs
@@ -8,8 +8,52 @@ namespace LearnCSharpExercises
 {
     internal class Program
     {
+        //No. 18 配列を入力値で初期化
+        //要素数10の整数型の配列を宣言し、整数値を入力させ、すべての配列の要素を入力値として、すべての要素の値を表示するプログラムを作成せよ。
+
+        //【実行例、下線部は入力例】
+        //$ ./knock18
+        //input number: 6
+        //6
+        //6
+        //6
+        //6
+        //6
+        //6
+        //6
+        //6
+        //6
+        //6
+        //$
+
         static void Main(string[] args)
         {
+            int num = 0;
+
+            while (num <= 0)
+            {
+                Console.Write("input number: ");
+                var val = Console.ReadLine();
+
+                if (int.TryParse(val, out num) == false)
+                {
+                    Console.WriteLine("整数を入力してください。");
+                    continue;
+                }
+            }
+
+            int[] list = new int[10];
+
+            // 繰り返しで初期値を代入する
+            for (int i = 0; i < list.Length; i++)
+            {
+                list[i] = num;
+            }
+
+            for (int i = 0; i < list.Length; i++)
+            {
+                Console.WriteLine(list[i]);
+            }
         }
     }
 }
